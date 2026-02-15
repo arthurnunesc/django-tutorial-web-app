742ffb6c472b07fc65893eba62e060d3e0e66c11
Arthur Nunes <arthurnunesc@proton.me>
2026-02-15 14:52:23 +0100

    Add first views

---
+++ todos/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('hello', views.hello_world_view, name='hello_world'),
+    path('', views.hello_python_view, name='hello_python'),
+]
--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def hello_world_view(request):
+    return HttpResponse("Hello World")
+
+def hello_python_view(request):
+    return HttpResponse("Hello Python")
